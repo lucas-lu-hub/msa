e62c0ae52c2a3bfc961cf5eeddd2ab303a80a98d
Lu Yuanbo <Lucas.Lu@grapecity.com>
2023-08-03 18:24:11 +0900

    初始化db

--- LucasNotes/DataBase/user_entrypoint.sh
+++
@@ -1,2 +0,0 @@
-# Run Microsoft SQl Server and initialization script (at the same time)
-/usr/src/app/user_run-initialization.sh & /opt/mssql/bin/sqlservr
--- LucasNotes/DataBase/user_v1.sql
+++ LucasNotes/DataBase/user_v1.sql
@@ -1,4 +1,3 @@
-DROP DATABASE  IF EXISTS [LucasNotes.UserDb]
 
 CREATE DATABASE [LucasNotes.UserDb]
 GO
